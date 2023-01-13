3d8d014825cd3ef25f143c6ed1e3613c94324be3
Yunus Emre EKE <yemreeke1@gmail.com>
2023-01-13 19:35:58 +0300

    HomeScreen design completed.

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,13 +1,24 @@
-import { Text, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { Alert, Image, Text, View } from "react-native";
 import BottomBar from "../components/BottomBar";
-import LocationScreen from "./LocationScreen";
+import { SCREENS } from "../navigation/navigation";
 
 const HomeScreen = () => {
+  const navigation = useNavigation()
   return (
-    <View style={{ flex: 1, }}>
-      <Text style={{ fontSize: 30, }}>hi</Text>
-      <LocationScreen></LocationScreen>
-      <BottomBar />
+    <View style={{ flex: 1, alignItems: "center", backgroundColor: "#fff" }}>
+      <Image style={{ marginTop: 30, height: 200, width: 200 }} source={require("../assets/img/logo.jpeg")} />
+      <Text style={{ marginTop: 10, fontSize: 30, color: "blue" }}>TRAKYA ÜNİVERSİTESİ</Text>
+      <Text style={{ marginTop: 10, fontSize: 26, color: "#000" }}>Bilgisayar Mühendisliği</Text>
+      <Text style={{ marginTop: 10, fontSize: 24, color: "#000" }}>Mobil Uygulama Dersi</Text>
+      <Text style={{ marginTop: 10, fontSize: 18, color: "#000" }}>Konum Öğrenip Sms İle Paylaşma Uygulaması</Text>
+      <Text style={{ marginTop: 20, fontSize: 26, color: "#000" }}>DOĞUKAN SONGÜR</Text>
+      <Text style={{ marginTop: 10, fontSize: 26, color: "#000" }}>1171602024</Text>
+      <BottomBar type="ONE"
+        onPress={() => {
+          navigation.navigate(SCREENS.LocationScreen);
+        }}
+      />
     </View >
   )
 }
