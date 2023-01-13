321876f0e3cd47505d624042120d9c36ac451540
Yunus Emre EKE <yemreeke1@gmail.com>
2023-01-13 19:01:44 +0300

    BottomBar components updated.

--- src/components/BottomBar.tsx
+++ src/components/BottomBar.tsx
@@ -1,5 +1,16 @@
 import { Text, TouchableOpacity, View } from "react-native"
-const BottomBar = () => {
+interface Props {
+  type: "ONE" | "TWO"
+  onPress?: () => void
+  disable?: boolean
+  nextDisable?: boolean
+  backDisable?: boolean
+  onNext?: () => void
+  onBack?: () => void
+}
+const BottomBar = (props: Props) => {
+  const { type, onPress, onBack, onNext, disable, nextDisable, backDisable } = props;
+  const disableColor = "#8c9da8";
   return (
     <View
       style={{
@@ -11,9 +22,21 @@ const BottomBar = () => {
         alignItems: "center"
       }}
     >
-      <TouchableOpacity style={{ height: 50, width: "94%", margin: "3%", borderRadius: 10, backgroundColor: "#5aa8da", alignItems: "center", justifyContent: "center", }}>
-        <Text style={{ fontSize: 25, color: "white", textAlign: "center" }}>İlerle</Text>
-      </TouchableOpacity>
+      {type == "ONE"
+        ?
+        <TouchableOpacity disabled={disable} onPress={onPress} style={{ height: 50, width: "94%", margin: "3%", borderRadius: 10, backgroundColor: disable ? disableColor : "#5aa8da", alignItems: "center", justifyContent: "center", }}>
+          <Text style={{ fontSize: 25, color: "white", textAlign: "center" }}>İlerle</Text>
+        </TouchableOpacity>
+        :
+        <>
+          <TouchableOpacity disabled={backDisable} onPress={onBack} style={{ height: 50, width: "44%", margin: "3%", borderRadius: 10, backgroundColor: disable ? disableColor : "#5aa8da", alignItems: "center", justifyContent: "center", }}>
+            <Text style={{ fontSize: 25, color: "white", textAlign: "center" }}>İlerle</Text>
+          </TouchableOpacity>
+          <TouchableOpacity disabled={nextDisable} onPress={onNext} style={{ height: 50, width: "44%", margin: "3%", borderRadius: 10, backgroundColor: disable ? disableColor : "#5aa8da", alignItems: "center", justifyContent: "center", }}>
+            <Text style={{ fontSize: 25, color: "white", textAlign: "center" }}>İlerle</Text>
+          </TouchableOpacity>
+        </>
+      }
     </View>
   )
 }
