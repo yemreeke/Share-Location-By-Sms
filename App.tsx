8bc05e9c68b9d9dfae49035f2739cfdfd38f4ef9
Yunus Emre EKE <yemreeke1@gmail.com>
2023-01-13 12:56:51 +0300

    navigation created.

--- App.tsx
+++ App.tsx
@@ -1,21 +1,11 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
 import React from 'react';
 import { SafeAreaView } from 'react-native';
-import Home from './src/screens/Home';
+import Navigation from './src/navigation/navigation';
 const App = () => {
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
-      <Home />
+      <Navigation />
     </SafeAreaView>
   );
 };
-
 export default App;
---
+++ src/navigation/navigation.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../screens/HomeScreen';
+import ContactScreen from '../screens/ContactScreen';
+import LocationScreen from '../screens/LocationScreen';
+
+const Stack = createNativeStackNavigator();
+
+
+
+
+const Navigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator >
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Ana Sayfa' }}
+        />
+        <Stack.Screen
+          name="LocationScreen"
+          component={LocationScreen}
+          options={{ title: 'Konum Ekranı' }}
+        />
+        <Stack.Screen
+          name="ContactScreen"
+          component={ContactScreen}
+          options={{ title: 'Kişi Seçme Ekranı' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+export default Navigation;
