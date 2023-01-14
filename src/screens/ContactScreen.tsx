f2f3e15dccfbdab26d853365079d9f7cf199d36c
Yunus Emre EKE <yemreeke1@gmail.com>
2023-01-14 22:27:26 +0300

    ContactScreen finished.

--- src/screens/ContactScreen.tsx
+++ src/screens/ContactScreen.tsx
@@ -1,8 +1,20 @@
-import { useState } from "react";
-import { Alert, Button, PermissionsAndroid, Platform, StyleSheet, Text, View } from "react-native";
-import { Contact, selectContact } from "react-native-select-contact";
-
-const ContactScreen = () => {
+import { useEffect, useState } from "react";
+import { Alert, PermissionsAndroid, Platform, StyleSheet, Text, View } from "react-native";
+import { Contact, selectContact, } from "react-native-select-contact";
+import BottomBar from "../components/BottomBar";
+import CustomButton from "../components/CustomButton";
+import SendSMS from "react-native-sms";
+interface Props {
+  navigation: any,
+  route: any
+}
+const ContactScreen = (props: Props) => {
+  const [mapUrl, setMapsUrl] = useState("")
+  useEffect(() => {
+    if (props.route.params.url) {
+      setMapsUrl(props.route.params.url)
+    }
+  }, [])
   const [selected, setSelected] = useState<Contact>();
   const selectPerson = async () => {
     if (await ContactPermission()) {
@@ -33,23 +45,45 @@ const ContactScreen = () => {
     }
     return true;
   };
-
+  const smsGonder = () => {
+    if (selected) {
+      const number = selected.phones[0].number.toString();
+      SendSMS.send(
+        {
+          body: mapUrl,
+          recipients: [number],
+          successTypes: ['sent', 'queued'],
+        },
+        (completed, cancelled, error) => {
+          if (completed) {
+            Alert.alert('SMS Başarıyla gönderilmiştir.');
+          } else if (cancelled) {
+            Alert.alert('SMS Gönderilmekten Vazgeçilmiştir');
+          } else if (error) {
+            Alert.alert('Some error occured' + error);
+          }
+        },
+      );
+    }
+  }
   return (
-    <View style={{ alignItems: "center", backgroundColor: "white" }}>
-      <View style={{ marginTop: 40, width: "50%" }}>
-        <Button
-          onPress={selectPerson} title="Kişi Seç" />
+    <View style={{ flex: 1, alignItems: "center", backgroundColor: "white", paddingTop: 30, }}>
+      <Text style={styles.header} >Sms Gönderilecek Kişiyi Seçiniz</Text>
+
+      <CustomButton text="Kişi Seç" width="50%" onPress={selectPerson} />
+      <View style={{ width: "100%", marginTop: 30, alignItems: "center" }}>
+        {
+          selected ?
+            <View style={{ width: "100%", alignItems: "center" }}>
+              <Text style={{ color: "black", fontSize: 25, marginBottom: 10, }}>Kişi Bilgileri</Text>
+              <Text style={{ color: "black", fontSize: 22, }} >Ad Soyad : {selected.name}</Text>
+              <Text style={{ color: "black", fontSize: 22, }} >Numara : {selected.phones[0].number}</Text>
+            </View>
+            :
+            <Text style={{ color: "red", fontSize: 24, }}>Kişi Seçilmemiştir!</Text>
+        }
       </View>
-      {
-        selected &&
-        <View style={{ marginTop: 30, alignItems: "center" }}>
-          <Text style={styles.title}>Seçilen Kişi</Text>
-          <Text style={styles.header} >Ad Soyad</Text>
-          <Text style={styles.text} >{selected.name}</Text>
-          <Text style={styles.header} >Numara</Text>
-          <Text style={styles.text} >{selected.phones[0].number}</Text>
-        </View>
-      }
+      <BottomBar disable={selected == undefined} text="Sms Gönder" onPress={smsGonder} />
     </View>
   )
 }
@@ -58,6 +92,7 @@ const styles = StyleSheet.create({
   title: {
     color: "black",
     fontSize: 25,
+    marginBottom: 10,
   },
   header: {
     color: "black",
