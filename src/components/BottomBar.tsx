7f68be4cef67012edb8dbd18a72dd302b67fb8f6
Yunus Emre EKE <yemreeke1@gmail.com>
2023-01-14 22:27:08 +0300

    BottomBar components updated.

--- src/components/BottomBar.tsx
+++ src/components/BottomBar.tsx
@@ -1,15 +1,11 @@
 import { Text, TouchableOpacity, View } from "react-native"
 interface Props {
-  type: "ONE" | "TWO"
   onPress?: () => void
   disable?: boolean
-  nextDisable?: boolean
-  backDisable?: boolean
-  onNext?: () => void
-  onBack?: () => void
+  text: string
 }
 const BottomBar = (props: Props) => {
-  const { type, onPress, onBack, onNext, disable, nextDisable, backDisable } = props;
+  const { onPress, disable, text } = props;
   const disableColor = "#8c9da8";
   return (
     <View
@@ -22,21 +18,9 @@ const BottomBar = (props: Props) => {
         alignItems: "center"
       }}
     >
-      {type == "ONE"
-        ?
-        <TouchableOpacity disabled={disable} onPress={onPress} style={{ height: 50, width: "94%", margin: "3%", borderRadius: 10, backgroundColor: disable ? disableColor : "#5aa8da", alignItems: "center", justifyContent: "center", }}>
-          <Text style={{ fontSize: 25, color: "white", textAlign: "center" }}>İlerle</Text>
-        </TouchableOpacity>
-        :
-        <>
-          <TouchableOpacity disabled={backDisable} onPress={onBack} style={{ height: 50, width: "44%", margin: "3%", borderRadius: 10, backgroundColor: disable ? disableColor : "#5aa8da", alignItems: "center", justifyContent: "center", }}>
-            <Text style={{ fontSize: 25, color: "white", textAlign: "center" }}>İlerle</Text>
-          </TouchableOpacity>
-          <TouchableOpacity disabled={nextDisable} onPress={onNext} style={{ height: 50, width: "44%", margin: "3%", borderRadius: 10, backgroundColor: disable ? disableColor : "#5aa8da", alignItems: "center", justifyContent: "center", }}>
-            <Text style={{ fontSize: 25, color: "white", textAlign: "center" }}>İlerle</Text>
-          </TouchableOpacity>
-        </>
-      }
+      <TouchableOpacity disabled={disable} onPress={onPress} style={{ height: 50, width: "94%", margin: "3%", borderRadius: 10, backgroundColor: disable ? disableColor : "#5aa8da", alignItems: "center", justifyContent: "center", }}>
+        <Text style={{ fontSize: 25, color: "white", textAlign: "center" }}>{text}</Text>
+      </TouchableOpacity>
     </View>
   )
 }
